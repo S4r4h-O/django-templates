c7ccec5fd21232cbbc4c5506d8f54708c73928f6
Sarah <sarah.shofani@proton.me>
2025-08-29 11:44:16 -0300

    First commit

---
+++ react-template/{{ cookiecutter.project_slug }}/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-lg text-succes">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
